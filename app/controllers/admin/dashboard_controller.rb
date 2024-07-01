3853ac2f012b115e683c6754e31ff35b4963c694
henrique lopes <henrique.0.lopes@hotmail.com>
2024-07-01 14:58:57 -0300

    Created dashboard to analytics

---
+++ app/controllers/admin/dashboard_controller.rb
@@ -0,0 +1,8 @@
+class Admin::DashboardController < ApplicationController
+  def index
+    @search_counts_by_ip = SearchAnalytic.group(:ip).count
+    @most_frequent_searches = SearchAnalytic.group(:query).order('count_query DESC').count(:query)
+    @searches_over_time = SearchAnalytic.group_by_day(:created_at).count
+    @recent_searches = SearchAnalytic.order(created_at: :desc).limit(10)
+  end
+end
